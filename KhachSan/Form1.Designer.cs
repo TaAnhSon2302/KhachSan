babd27fe798fd905b1290b35f9b303a27d6552b1
Admin <2051060687@e.tlu.edu.vn>
2022-06-08 21:25:25 +0700

    Add project files.

---
+++ KhachSan/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace KhachSan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtQuanity = new System.Windows.Forms.TextBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.cbType = new System.Windows.Forms.ComboBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnFix = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.rdbNam = new System.Windows.Forms.RadioButton();
+            this.rdbNu = new System.Windows.Forms.RadioButton();
+            this.Stt = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TenKH = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.GioiTinh = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LoaiPhong = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SLPhong = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 14);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tên khách hàng";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 14);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Giới tính";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 14);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Loại phòng";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 147);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 14);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Số phòng thuê";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(455, 30);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(25, 14);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Tên";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(458, 64);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 5;
+            this.btnSearch.Text = "Tim kiếm";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(143, 24);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(208, 20);
+            this.txtName.TabIndex = 6;
+            // 
+            // txtQuanity
+            // 
+            this.txtQuanity.Location = new System.Drawing.Point(143, 144);
+            this.txtQuanity.Name = "txtQuanity";
+            this.txtQuanity.Size = new System.Drawing.Size(208, 20);
+            this.txtQuanity.TabIndex = 7;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(503, 27);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(100, 20);
+            this.txtSearch.TabIndex = 8;
+            // 
+            // cbType
+            // 
+            this.cbType.FormattingEnabled = true;
+            this.cbType.Items.AddRange(new object[] {
+            "Phòng đơn",
+            "Phòng đôi"});
+            this.cbType.Location = new System.Drawing.Point(140, 107);
+            this.cbType.Name = "cbType";
+            this.cbType.Size = new System.Drawing.Size(86, 22);
+            this.cbType.TabIndex = 11;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(405, 156);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 12;
+            this.btnAdd.Text = "Thêm";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(528, 156);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 13;
+            this.btnDelete.Text = "Xóa";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnFix
+            // 
+            this.btnFix.Location = new System.Drawing.Point(645, 156);
+            this.btnFix.Name = "btnFix";
+            this.btnFix.Size = new System.Drawing.Size(75, 23);
+            this.btnFix.TabIndex = 14;
+            this.btnFix.Text = "Sửa";
+            this.btnFix.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Stt,
+            this.TenKH,
+            this.GioiTinh,
+            this.LoaiPhong,
+            this.SLPhong});
+            this.dataGridView1.Location = new System.Drawing.Point(37, 196);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(683, 209);
+            this.dataGridView1.TabIndex = 15;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // rdbNam
+            // 
+            this.rdbNam.AutoSize = true;
+            this.rdbNam.Location = new System.Drawing.Point(143, 66);
+            this.rdbNam.Name = "rdbNam";
+            this.rdbNam.Size = new System.Drawing.Size(46, 18);
+            this.rdbNam.TabIndex = 16;
+            this.rdbNam.TabStop = true;
+            this.rdbNam.Text = "Nam";
+            this.rdbNam.UseVisualStyleBackColor = true;
+            // 
+            // rdbNu
+            // 
+            this.rdbNu.AutoSize = true;
+            this.rdbNu.Location = new System.Drawing.Point(213, 66);
+            this.rdbNu.Name = "rdbNu";
+            this.rdbNu.Size = new System.Drawing.Size(39, 18);
+            this.rdbNu.TabIndex = 17;
+            this.rdbNu.TabStop = true;
+            this.rdbNu.Text = "Nữ";
+            this.rdbNu.UseVisualStyleBackColor = true;
+            // 
+            // Stt
+            // 
+            this.Stt.DataPropertyName = "Stt";
+            this.Stt.HeaderText = "Stt";
+            this.Stt.Name = "Stt";
+            this.Stt.Width = 30;
+            // 
+            // TenKH
+            // 
+            this.TenKH.DataPropertyName = "TenKH";
+            this.TenKH.HeaderText = "Tên khách hàng";
+            this.TenKH.Name = "TenKH";
+            this.TenKH.Width = 130;
+            // 
+            // GioiTinh
+            // 
+            this.GioiTinh.DataPropertyName = "GioiTinh";
+            this.GioiTinh.HeaderText = "Giới Tính";
+            this.GioiTinh.Name = "GioiTinh";
+            // 
+            // LoaiPhong
+            // 
+            this.LoaiPhong.DataPropertyName = "LoaiPhong";
+            this.LoaiPhong.HeaderText = "Loại phòng";
+            this.LoaiPhong.Name = "LoaiPhong";
+            // 
+            // SLPhong
+            // 
+            this.SLPhong.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.SLPhong.DataPropertyName = "SLPhong";
+            this.SLPhong.HeaderText = "Số phòng cần thuê";
+            this.SLPhong.Name = "SLPhong";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rdbNu);
+            this.Controls.Add(this.rdbNam);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnFix);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.cbType);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.txtQuanity);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "QL khách hàng trong khách sạn(ho tên - msv)";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtQuanity;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.ComboBox cbType;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnFix;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.RadioButton rdbNam;
+        private System.Windows.Forms.RadioButton rdbNu;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Stt;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TenKH;
+        private System.Windows.Forms.DataGridViewTextBoxColumn GioiTinh;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LoaiPhong;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SLPhong;
+    }
+}
+
---
+++ KhachSan/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace KhachSan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection cn;
+        SqlCommand cmd,cmd2;
+        string sql= @"Data Source=H114\SQLEXPRESS;Initial Catalog=Pratice;Integrated Security=True";
+        SqlDataAdapter adapter = new SqlDataAdapter();
+        DataTable dt = new DataTable();
+        void loaddata()
+        {
+            cmd = cn.CreateCommand();
+            cmd.CommandText = "Select ROW_NUMBER() Over (Order by TenKH) as [Stt], TenKH,GioiTinh,LoaiPhong,SLPhong from dbo.KhachSan";
+            adapter.SelectCommand = cmd;
+            dt.Clear();
+            adapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cn = new SqlConnection(sql);
+            cn.Open();
+            cmd = cn.CreateCommand();
+            cmd.CommandText = "Select ROW_NUMBER() Over (Order by TenKH) as [Stt], TenKH,GioiTinh,LoaiPhong,SLPhong from dbo.KhachSan";
+            adapter.SelectCommand = cmd;
+            dt.Clear();
+            adapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            cn = new SqlConnection(sql);
+            cn.Open();
+            cmd=cn.CreateCommand();
+            int a = int.Parse(txtQuanity.Text.ToString());
+            string gt = "";
+            if (rdbNam.Checked)
+            {
+                gt = "Nam";
+            }
+            if(rdbNu.Checked)
+            {
+                gt = "Nữ";
+            }    
+            cmd.CommandText = "Insert into KhachSan values(N'"+txtName.Text+"',N'"+gt+"',N'"+cbType.Text+"','"+a+"')";
+            cmd.ExecuteNonQuery();
+            loaddata();
+            foreach (Control c in this.Controls)
+            {
+                if (c is TextBox)
+                {
+                    c.Text = "";
+                }
+            }
+            rdbNam.Checked = false;
+            rdbNu.Checked = false;
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            cn = new SqlConnection(sql);
+            cn.Open();
+            cmd = cn.CreateCommand();
+            cmd.CommandText = "Delete from KhachSan where TenKH=N'" + txtName.Text + "'";
+            cmd.ExecuteNonQuery();
+           
+        }
+
+        private void Form1_DoubleClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            cn = new SqlConnection(sql);
+            cn.Open();
+            cmd = cn.CreateCommand();
+            cmd.CommandText = "Select ROW_NUMBER() Over (Order by TenKH) as [Stt], TenKH,GioiTinh,LoaiPhong,SLPhong from dbo.KhachSan where TenKH=N'" + txtSearch.Text+"' or LoaiPhong=N'"+txtSearch.Text+"' or GioiTinh=N'"+txtSearch.Text+"'";
+            adapter.SelectCommand = cmd;
+            dt.Clear();
+            adapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int i = dataGridView1.CurrentRow.Index;
+            txtName.Text = dataGridView1.Rows[i].Cells[1].Value.ToString();
+            cbType.Text = dataGridView1.Rows[i].Cells[3].Value.ToString();
+            txtQuanity.Text = dataGridView1.Rows[i].Cells[4].Value.ToString();
+            string gt = dataGridView1.Rows[i].Cells[2].Value.ToString();
+            if(gt == "Nam")
+            {
+                rdbNam.Checked = true;
+            }
+            if (gt == "Nữ")
+            {
+                rdbNam.Checked = true;
+            }
+
+        }
+    }
+}
